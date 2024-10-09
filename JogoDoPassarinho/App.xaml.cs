bb637eba354769f02b8e8f38f3e3a0ca8dd5f080
amabily <amabilymangolin123@gmail.com>
2024-10-09 08:39:33 -0300

    começo

---
+++ JogoDoPassarinho/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace JogoDoPassarinho;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ JogoDoPassarinho/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace JogoDoPassarinho;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
