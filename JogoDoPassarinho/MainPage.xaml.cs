f0fd41df7ce8f4faa5ecfbd05b685ec2b0f15fc8
amabily <amabilymangolin123@gmail.com>
2024-10-15 08:54:13 -0300

    .

--- JogoDoPassarinho/MainPage.xaml.cs
+++ JogoDoPassarinho/MainPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace JogoDoPassarinho;
+﻿using System.Threading.Channels;
+
+namespace JogoDoPassarinho;
 
 public partial class MainPage : ContentPage
 {
@@ -8,5 +10,48 @@ public partial class MainPage : ContentPage
 		InitializeComponent();
 	}
 
+	bool VerificaColisao()
+	{
+		if(!estaMorto)
+	
+	{
+	if(VerificaColisaoTeto() ||
+	   VerificaColisaoChao())
+	   {
+		return true;
+	   }
+    }
+		return false;
+ }
+	bool VerificaColisaoTeto()
+	{
+		var minY = -alturaJanela/2;
+		if (paimon.TranslationY <= minY)
+		return true;
+	else
+		return false;
+	}
+	bool VerificaColisaoChao()
+	{
+		var maxY = alturaJanela/2 - graminha.HeightRequest;
+		if (paimon.TranslationY >= maxY)
+		return true;
+	else
+		return false;
+	}
+	async Task Desenha()
+	{
+		while (!estaMorto)
+		{
+			AplicaGravidade();
+			GerenciaCanos();
+			if (VerificaColisao())
+			{
+				estaMorto = true;
+				FrameGameOver.IsVisible = true;
+				break;
+			}
+			await Task.Delay(tempoEntreFrames);
+		}
+	}
 }
-
