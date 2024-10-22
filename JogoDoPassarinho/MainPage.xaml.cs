79343900ed70c81e3ec775ed105e40cf0843ee56
amabily <amabilymangolin123@gmail.com>
2024-10-22 09:02:37 -0300

    .

--- JogoDoPassarinho/MainPage.xaml.cs
+++ JogoDoPassarinho/MainPage.xaml.cs
@@ -5,7 +5,7 @@ namespace JogoDoPassarinho;
 public partial class MainPage : ContentPage
 {
 
-const int gravidade = 7;
+	const int gravidade = 4;
 	const int tempoEntreFrames = 25;
 	bool estaMorto = false;
 	double larguraJanela = 0;
@@ -16,7 +16,7 @@ const int gravidade = 7;
 	int tempoPulando = 0;
 	bool estaPulando = false;
 	int score = 0;
-	const int aberturaMinima = 200;
+	const int aberturaMinima = 250;
 
 
 
@@ -35,16 +35,16 @@ const int gravidade = 7;
 			pilarnormal.TranslationX = 0;
 			var alturaMax = -100;
 			var alturaMin = -pilarvirado.HeightRequest;
-			pilarnormal.TranslationY = Random.Shared.Next((int)alturaMin, (int) alturaMax);
+			pilarnormal.TranslationY = Random.Shared.Next((int)alturaMin, (int)alturaMax);
 			pilarvirado.TranslationY = pilarnormal.TranslationY + aberturaMinima + pilarvirado.HeightRequest;
-			score ++;
+			score++;
 			labelScore.Text = "Canos: " + score.ToString("D3");
 			ok.Text = "voce morreu mas passou por " + score.ToString("D3") + " canos tente novamente!";
 		}
 	}
 
 	protected override void OnSizeAllocated(double w, double h)
-    {
+	{
 		base.OnSizeAllocated(w, h);
 		larguraJanela = w;
 		alturaJanela = h;
@@ -52,8 +52,13 @@ const int gravidade = 7;
 
 	void Inicializar()
 	{
+		pilarnormal.TranslationX = -larguraJanela;
+		pilarvirado.TranslationX = -larguraJanela;
+		paimon.TranslationX = 0;
 		paimon.TranslationY = 0;
 		score = 0;
+		GerenciaCanos();
+	
 	}
 
 	void Oi(object s, TappedEventArgs e)
@@ -84,10 +89,10 @@ const int gravidade = 7;
 	{
 		while (!estaMorto)
 		{
-			if(estaPulando)
-			  AplicaPulo();
+			if (estaPulando)
+				AplicaPulo();
 			else
-			AplicaGravidade();
+				AplicaGravidade();
 			GerenciaCanos();
 			if (VerificaColisao())
 			{
@@ -100,12 +105,14 @@ const int gravidade = 7;
 	}
 
 	bool VerificaColisao()
-	{
+   {
 		if(!estaMorto)
+
 	
 	{
 	if(VerificaColisaoTeto() ||
-	   VerificaColisaoChao())
+	   VerificaColisaoChao() ||
+	   VerificaColisaopilarnormal())
 	   {
 		return true;
 	   }
@@ -113,24 +120,40 @@ const int gravidade = 7;
 		return false;
  }
 	bool VerificaColisaoTeto()
-	{
-		var minY = -alturaJanela/2;
-		if (paimon.TranslationY <= minY)
+{
+	var minY = -alturaJanela / 2;
+	if (paimon.TranslationY <= minY)
 		return true;
 	else
 		return false;
-	}
-	bool VerificaColisaoChao()
-	{
-		var maxY = alturaJanela/2 - graminha.HeightRequest;
-		if (paimon.TranslationY >= maxY)
+}
+bool VerificaColisaoChao()
+{
+	var maxY = alturaJanela / 2 - graminha.HeightRequest;
+	if (paimon.TranslationY >= maxY)
 		return true;
 	else
 		return false;
-	}
+}
 
-	void OnGridClicked(object s, TappedEventArgs a)
-	{
-		estaPulando = true;
+void OnGridClicked(object s, TappedEventArgs a)
+{
+	estaPulando = true;
+}
+
+bool VerificaColisaopilarnormal()
+   {
+	var posHpaimon = (larguraJanela / 2) - (paimon.WidthRequest / 2);
+	var posVpaimon =  (alturaJanela/2) - (paimon.HeightRequest/2) + paimon.TranslationY;
+		if (posHpaimon >= Math.Abs(pilarnormal.TranslationX) - pilarnormal.WidthRequest&&
+		posHpaimon <= Math.Abs(pilarnormal.TranslationX) + pilarvirado.WidthRequest&&
+		posVpaimon <= pilarnormal.HeightRequest + pilarnormal.TranslationY)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
 	}
 }
